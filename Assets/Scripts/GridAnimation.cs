14d2438c8920776a7cf9c1d2d134d14c694ecb82
Sergey <poshta-nekrasov@yandex.ru>
2020-01-11 20:27:58 +0300

    Almost done

--- Assets/Scripts/GridAnimation.cs
+++ Assets/Scripts/GridAnimation.cs
@@ -1,6 +1,4 @@
 ﻿// Attach to Main Camera
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -12,14 +10,24 @@ public class GridAnimation : MonoBehaviour
     [SerializeField] private int LeftY;
     [SerializeField] private int GridHeight;
     [SerializeField] private int GridWidth;
+    [SerializeField] private Tilemap Spikes;
+    [SerializeField] private Tile Spike;
+    [SerializeField] private GameObject Ghost;
+    private int LastX;
+    private int CurrentEnemyCoord;
 
     void Start()
     {
+        LastX = LeftX;
+        CurrentEnemyCoord = LastX + GridWidth;
+        GenerateSpikes(PrerenderDistance);
         if (PrerenderDistance > 0)
         {
             foreach (Transform tilemapTransform in GridObject.transform)
             {
                 Tilemap tilemap = tilemapTransform.gameObject.GetComponent<Tilemap>();
+                // Skip spikes layer
+                if (tilemap.name == "Spikes") continue;
                 for (int i = GridWidth + LeftX; i < GridWidth + PrerenderDistance + LeftX; i++)
                 {
                     for (int j = LeftY; j > LeftY - GridHeight; j--)
@@ -29,11 +37,126 @@ public class GridAnimation : MonoBehaviour
                     }
                 }
             }
+            LeftX += PrerenderDistance + GridWidth - 1;
         }
     }
 
-    void Update()
+    public void GenerateBackground(int calls)
     {
-        
+        while (calls > 0)
+        {
+            foreach (Transform tilemapTransform in GridObject.transform)
+            {
+                Tilemap tilemap = tilemapTransform.gameObject.GetComponent<Tilemap>();
+                // Skip spikes layer
+                if (tilemap.name == "Spikes") continue;
+                for (int j = LeftY; j > LeftY - GridHeight; j--)
+                {
+                    TileBase replaceTile = tilemap.GetTile(new Vector3Int(LeftX - GridWidth + 1, j, 0));
+                    tilemap.SetTile(new Vector3Int(LeftX + 1, j, 0), replaceTile);
+                }
+            }
+            LeftX += 1;
+            calls -= 1;
+        }       
+    }
+
+    public void ClearUnusedBackground(int leftX)
+    {
+        foreach (Transform tilemapTransform in GridObject.transform)
+        {
+            Tilemap tilemap = tilemapTransform.gameObject.GetComponent<Tilemap>();
+            for (int i = LastX; i < leftX - GridWidth; i++)
+            {
+                for (int j = LeftY; j > LeftY - GridHeight; j--)
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), null);
+                }
+            }
+        }
+        LastX = leftX - GridWidth;
+    }
+
+    public void GenerateEnemies(int length)
+    {   while (length > 0)
+        {
+            int levelLength;
+            int levelChoice = Random.Range(1, 5);
+            if (length > 20)
+            {
+                levelLength = Random.Range(5, 21);
+                length -= levelLength;
+            }
+            else
+            {
+                levelLength = Random.Range(1, length + 1);
+                length -= levelLength;
+            }
+            if (levelChoice >= 3)
+            {
+                GenerateGhosts(levelLength);
+            }
+            else
+            {
+                GenerateSpikes(levelLength);
+            }
+        }        
+    }
+
+    public void GenerateSpikes(int length)
+    {
+        // One empty tile at start
+        CurrentEnemyCoord += 1;
+        length -= 1;
+        while (length > 0)
+        {
+            if (length > 5)
+            {
+                int currentLineLength = Random.Range(1, 6);
+                length -= currentLineLength;
+                while (currentLineLength > 0)
+                {
+                    Spikes.SetTile(new Vector3Int(CurrentEnemyCoord, -3, 0), Spike);
+                    CurrentEnemyCoord++;
+                    currentLineLength--;
+
+                }
+                CurrentEnemyCoord += Random.Range(5, 10);
+            }
+            else
+            {
+                // Pass last empty
+                CurrentEnemyCoord += length;
+                length = 0;
+            }          
+        }
+    }
+
+    public void GenerateGhosts(int length)
+    {
+        // One empty tile at start
+        CurrentEnemyCoord += 1;
+        length -= 1;
+        int fixedPos = 0;
+        while (length > 0)
+        {
+            float height;
+            if (fixedPos > 0)
+            {
+                height = Random.Range(-0.2f, 0.4f);
+                fixedPos -= 1;
+            }
+            else
+            {
+                height = Random.Range(-0.8f, 0.4f);
+            }           
+            Instantiate(Ghost, new Vector3((CurrentEnemyCoord * 0.33f) + 1.577f, height, 0), new Quaternion(0, 0, 0, 0));
+            if (height < -0.2f)
+            {
+                fixedPos = 2;
+            }
+            length -= 6;
+            CurrentEnemyCoord += 5;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,135 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float Speed;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private float FallForce;
+    [SerializeField] private float TimeToSpeedUp;
+    [SerializeField] private int PregenerateDistance;
+    private Animator Animator;
+    private GridAnimation GridAnimator;
+    private GameObject MainCamera;
+    private Rigidbody2D Rb;
+    private bool TimerCheck = true;
+    private float StepPosition;
+    private float groundPosY;
+    private bool InputJump;
+    private float CameraStartX;
+    private bool ButtonHold = false;
+    private float RunSpeed = 0.7f;
+    private float JumpSpeed = 1.0f;
+    private bool Alive = true;
+
+    private void Start()
     {
-        
+        Animator = gameObject.GetComponent<Animator>();
+        groundPosY = gameObject.transform.position.y;
+        Rb = gameObject.GetComponent<Rigidbody2D>();
+        GridAnimator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GridAnimation>();
+        MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        CameraStartX = MainCamera.transform.position.x;
+        StepPosition = gameObject.transform.position.x;
+        Animator.SetBool("Run", true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Alive)
+        {
+            Rb.velocity = new Vector2(Speed, Rb.velocity.y);
+        }
+        MainCamera.transform.position = new Vector3(CameraStartX + gameObject.transform.position.x, MainCamera.transform.position.y, MainCamera.transform.position.z);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && transform.position.y <= groundPosY + 0.05f)
+        {
+            InputJump = true;
+            Animator.SetTrigger("Jump");
+            StartCoroutine("Jump");
+        }
+        if (Input.GetMouseButton(0))
+        {
+            ButtonHold = true;
+        }
+        else
+        {
+            ButtonHold = false;
+        }
+        if (gameObject.transform.position.x - StepPosition >= PregenerateDistance)
+        {
+            GridAnimator.GenerateBackground(75);
+            GridAnimator.GenerateEnemies(75);
+            GridAnimator.ClearUnusedBackground(Convert.ToInt32(transform.position.x * 3) - 6);
+            StepPosition = gameObject.transform.position.x;
+        }
+        if (TimerCheck && Speed < 15f)
+        {
+            StartCoroutine(SpeedIncrease());
+        }
+    }
+
+    IEnumerator SpeedIncrease()
+    {
+        TimerCheck = false;
+        Speed += 0.1f;
+        JumpForce += 0.1f;
+        FallForce += 0.1f;
+        RunSpeed += 0.01f;
+        JumpSpeed += 0.1f;
+        Animator.SetFloat("RunSpeed", RunSpeed);
+        Animator.SetFloat("JumpSpeed", RunSpeed);
+        yield return new WaitForSeconds(TimeToSpeedUp);
+        TimerCheck = true;
+    }
+
+    IEnumerator Jump()
+    {
+        while (true)
+        {           
+            if (transform.position.y >= 0.45f)
+            {
+                InputJump = false;
+            }               
+            if (InputJump)
+            {
+                transform.Translate(Vector3.up * JumpForce * Time.smoothDeltaTime);
+            }               
+            else if (!InputJump)
+            {
+                transform.Translate(Vector3.down * (FallForce / (1f + (2f * System.Convert.ToSingle(ButtonHold)))) * Time.smoothDeltaTime);
+                if (ButtonHold)
+                {
+                    Animator.SetFloat("JumpSpeed", JumpSpeed * 0.4f);
+                }
+                else
+                {
+                    Animator.SetFloat("JumpSpeed", JumpSpeed);
+                }
+                if (transform.position.y < groundPosY)
+                {
+                    transform.position = new Vector2(transform.position.x, groundPosY);                    
+                    StopCoroutine("Jump");
+                }
+            }
+            yield return new WaitForEndOfFrame();           
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && Alive)
+        {
+            Alive = false;
+            Animator.SetBool("Run", false);
+            Rb.velocity = new Vector2(0, Rb.velocity.y);
+            Animator.SetTrigger("Death");
+            gameObject.GetComponent<PlayerController>().enabled = false;
+        }
     }
 }
